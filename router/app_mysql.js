1ab20566b4aa8be35b9dd7cf0e238e7e908ebb31
hywoong <woong9423@gmail.com>
2016-11-08 03:32:24 +0900

    mysql test sucess

--- router/app_mysql.js
+++ router/app_mysql.js
@@ -2,11 +2,42 @@
  * Created by hyw on 2016-11-08.
  */
 var express = require("express");
-var bodyParser = require("bodyParser");
+var bodyParser = require("body-parser");
 var mysql = require('mysql');
 //multer : file upload
+var connection = mysql.createConnection({
+    /*host : 'localhost',
+    user : 'root',
+    password : '1234',
+    database : 'sourgrape'*/
+    host : 'localhost',
+    user : 'root',
+    password : 'hywoong94!',
+    database : 'sourgrape'
+});
 var app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.locals.pretty = true;
 
+
+connection.connect(function (err){
+   if(err){
+       console.log("Error Connecting DataBase ...\n");
+   }
+   else{
+       console.log("DataBase is Connected !!!\n");
+   }
+});
+
+app.get('/test', function (req,res) {
+    var sql = 'select * from game';
+    connection.query(sql,function (err, rows, fields){
+        //res.send(rows);
+        res.json(rows);
+    });
+});
+
+app.listen(3000,function(){
+   console.log("connection 3000 port");
+});
