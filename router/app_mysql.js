d9f303f3b11384e0b62a0bcdef2e21d7f91350e9
hywoong <woong9423@gmail.com>
2016-11-19 00:13:13 +0900

    game/:title game-rate/:title 추가.

--- router/app_mysql.js
+++ router/app_mysql.js
@@ -46,7 +46,21 @@ app.get('/games', function (req, res) {
     });
 });
 app.get('/game-rate', function (req, res) {
-    var sql = 'SELECT gr_title, gr_id, round(avg(rate)/2,1) as rate, rate_date FROM game_rate group by gr_title;';
+    var sql = 'SELECT gr_title, gr_id, round(avg(rate)/2,1) as rate, rate_date FROM game_rate group by gr_title order by rate desc;';
+    connection.query(sql, function (err, rows, fields) {
+        //res.send(rows);
+        res.json(rows);
+    });
+});
+app.get('/game/:title', function (req, res) {
+    var sql = 'select * from game where title = \'' + req.params.title + '\'';
+    connection.query(sql, function (err, rows, fields) {
+        //res.send(rows);
+        res.json(rows);
+    });
+});
+app.get('/game-rate/:title', function (req, res) {
+    var sql = 'select * from game_rate where gr_title = \'' + req.params.title + '\'';
     connection.query(sql, function (err, rows, fields) {
         //res.send(rows);
         res.json(rows);
