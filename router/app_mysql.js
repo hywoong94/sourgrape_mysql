fcf1fdcb648918faf3cecd821b56f29687c250e5
hywoong <woong9423@gmail.com>
2016-11-26 05:51:04 +0900

    /users/:id/:num 수정2

--- router/app_mysql.js
+++ router/app_mysql.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
     user: 'root',
     password: '1234',
     database: 'testhost'
-    //database: 'sourgrape'
+    // database: 'sourgrape'
 
     // host: 'localhost',
     // user: 'root',
