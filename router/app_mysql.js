9f9c6533c6454edae075291f7c3c2fec591467b4
hywoong <woong9423@gmail.com>
2016-11-29 13:53:45 +0900

    테스트 데이터 연결

--- router/app_mysql.js
+++ router/app_mysql.js
@@ -10,13 +10,13 @@ var connection = mysql.createConnection({
     // host: 'localhost',
     // user: 'root',
     // password: '1234',
-    // // database: 'testhost'
+    database: 'testhost'
     // database: 'sourgrape'
 
-    host: 'localhost',
-    user: 'root',
-    password: 'hywoong94!',
-    database: 'sourgrape'
+    // host: 'localhost',
+    // user: 'root',
+    // password: 'hywoong94!',
+    // database: 'sourgrape'
 });
 var app = express();
 
